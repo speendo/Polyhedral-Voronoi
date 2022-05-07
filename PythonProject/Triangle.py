ae82fb264781023a009c901c73017b7417ae8d79
Marcel Jira <marcel.jira@gmail.com>
2022-05-07 23:51:50 +0200

    Do a big refactor - scatter not yet working

--- PythonProject/Triangle.py
+++ PythonProject/Triangle.py
@@ -80,7 +80,6 @@ class Triangle:
         return (point.y() - self.center.y()) / Triangle.base_height
 
     def store_top_collision(self):
-<<<<<<< HEAD
         if not self.top_points:
             # ToDo: handle no collisions
             self.top_collision = Collision(point=None, scale=float('inf'), has_happened=True)
@@ -134,26 +133,3 @@ class Triangles:
 
     def find_next_collision(self) -> Collision:
         return min(self.triangles, key=lambda t: t.find_next_collision().scale)
-=======
-        top_collision_point = min(self.top_points, key=lambda p: p.y())
-        top_scale_to_hit = self.calc_top_scale(self.top_collision_point)
-        self.top_collision = Collision(top_collision_point, top_scale_to_hit)
-
-    def store_left_collision(self):
-        self.left_collision = Collision(None, float('inf'))
-        for p in self.left_points:
-            cur_scale = self.calc_side_scale(p)
-            if cur_scale < self.left_collision.scale:
-                self.left_collision = Collision(p, cur_scale)
-
-    def store_right_collision(self):
-        self.right_collision = Collision(None, float('inf'))
-        for p in self.right_points:
-            cur_scale = self.calc_side_scale(p)
-            if cur_scale < self.right_collision.scale:
-                self.right_collision = Collision(p, cur_scale)
-
-    def find_next_collision(self) -> Collision:
-        return min([self.top_collision, self.left_collision, self.right_collision],
-                   key=lambda c: float('inf') if c.has_happened else c.scale)
->>>>>>> origin/Marcel's_Refactor
